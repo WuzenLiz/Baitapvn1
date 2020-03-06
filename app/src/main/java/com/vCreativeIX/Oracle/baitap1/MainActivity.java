15b4bc4e0f0f535869333ba90b6ec46d4233029a
Zen  the Oracle <Macgia99@outlook.com>
2020-03-06 23:16:24 +0700

    Update :))

--- app/src/main/java/com/vCreativeIX/Oracle/baitap1/MainActivity.java
+++ app/src/main/java/com/vCreativeIX/Oracle/baitap1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,12 +28,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setContentView(R.layout.activity_main);
         //Define
-        btnInput = (Button) findViewById(R.id.btnInput);
-        btnUpdate = (Button) findViewById(R.id.btnUpdate);
-        SubList = (ListView) findViewById(R.id.SubView);
-        subInp = (EditText) findViewById(R.id.SubInp);
-        list = new ArrayList<String>();
+        btnInput = findViewById(R.id.btnInput);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        SubList = findViewById(R.id.SubView);
+        subInp = findViewById(R.id.SubInp);
+        list = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
         //Function_Define
         btnInput.setOnClickListener(new View.OnClickListener() {
